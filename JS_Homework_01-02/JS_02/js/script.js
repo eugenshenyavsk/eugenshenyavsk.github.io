e297ddb844f15ee9e3d0b568c38775dd7c8fbffd
Eugene <zhmenshenyavsk@gmail.com>
2017-07-21 19:24:06 +0300

    new homework

--- JS_Homework_01-02/JS_02/js/script.js
+++ JS_Homework_01-02/JS_02/js/script.js
@@ -1,46 +1,16 @@
-var login = (function () {
-    var userNames = [];
-    var userName,
-        found;
+var arrName = [];
 
-    var addUserNames = function () {
-        var number;
+for (var i = 0; i < 5; i++) {
+    arrName[i] = prompt('Введите имя', '');
+}
+var login = prompt ('Введите имя пользователя');
+var flag = false;
 
-        for (number = 1; number <= 5; number++) {
-            newName = prompt('Please, enter a name # ' + number);
-            userNames.push(newName);
-        }
-    };
-
-    var askUserName = function () {
-        userName = prompt('Please, enter your name');
-    };
-
-    var checkUserName = function () {
-        found = false;
-        var i;
-        var length = userNames.length;
-
-        for (i = 0; i <= length; i++) {
-            if (userName == userNames[i]) {
-                found = true;
-            }
-        }
-
-        if (!found) {
-            document.write(userName, ', you\'re not allowed!');
-        } else {
-            document.write(userName, ', welcome to the website!');
-        }
-    };
-
-    return {
-        addUserNames: addUserNames,
-        askUserName: askUserName,
-        checkUserName: checkUserName
+for (var i = 0; i < arrName.length; i++ ) {
+    if (login === arrName[i]) {
+        alert(login + ', вы успешно вошли');
     }
-})();
-
-login.addUserNames();
-login.askUserName();
-login.checkUserName();
+    else {
+        alert('Ошибка входа!');
+    }
+}
